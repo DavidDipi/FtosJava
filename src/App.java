d55bd5f9c92f3bf7d1f82b4afaf9bc4e4b2e377b
David Diaz <daviddipi27@gmail.com>
2023-02-22 19:39:59 -0500

    [prueba de pc]

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre");
+        String name;
+        name=lectura.nextLine();
+
+        System.out.println("Ingresa un número");
+
+        int numberOne;
+        numberOne = lectura.nextInt();
+
+        System.out.println("Ingresa un segundo número");
+        int numberTwo;
+        numberTwo = lectura.nextInt();
+
+        int addition;
+        addition=numberOne+numberTwo;
+
+        System.out.println("¡Hola " + name + "!, ingresaste los números " + numberOne + " y " + numberTwo + " y el resultado de la suma es: " + addition);
+        
+        lectura.nextLine();//Limpieza de buffer  
+
+        lectura.close();
+    
     }
 }
