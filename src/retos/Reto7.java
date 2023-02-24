dd03983112381ee072fc0836645aa76c00e73ccc
David Diaz <daviddipi27@gmail.com>
2023-02-23 19:03:22 -0500

    [reto7]

---
+++ src/retos/Reto7.java
@@ -0,0 +1,66 @@
+package retos;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Reto7 {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        Random ramdom = new Random();
+
+        int valor, x;
+        Object resultado;
+
+        System.out.println("Ingrese el valor de la compra");
+        valor = lectura.nextInt();
+
+        if(valor >= 50000){
+
+            ArrayList bolitas = new ArrayList<>();
+
+            bolitas.add("rojo");
+            bolitas.add("amarillo");
+            bolitas.add("azul");
+            bolitas.add("blanco");
+
+            System.out.println(bolitas);
+
+            x = (int) (Math.random() * 3);
+
+            resultado = bolitas.get(x);
+
+            System.out.println("Valor elegido: " + resultado);
+
+            if(resultado == "rojo"){
+
+                System.out.println("Ha salido: " + resultado);
+                System.err.println("Has obtenido el descuento del 10% sobre el valor de tu compra");
+
+            }else if(resultado == "amarillo"){
+
+                System.out.println("Ha salido: " + resultado);
+                System.err.println("Has obtenido el descuento del 50% sobre el valor de tu compra");
+
+            }else if(resultado == "azul"){
+
+                System.out.println("Ha salido: " + resultado);
+                System.err.println("Has obtenido el descuento del 30% sobre el valor de tu compra");
+
+            }else if(resultado == "blanco"){
+
+                System.out.println("Ha salido: " + resultado);
+                System.err.println("Has obtenido tu compra gratis");
+
+            }else{
+                System.out.println("Ha ocurrido un error inesperado");
+            }
+
+
+        }else{
+            System.out.println("El valor de la compra es inferior de $50.000");
+        }
+
+        lectura.close();
+    }
+}
